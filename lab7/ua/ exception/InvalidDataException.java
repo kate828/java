f44f9a55810ac9debb28c1e8876ca64c1c6b3531
kate828 <katepenziy10@gmail.com>
2025-11-11 17:36:27 +0200

    Create InvalidDataException.java

---
+++ lab7/ua/ exception/InvalidDataException.java
@@ -0,0 +1,12 @@
+package ua.exception;
+
+public class InvalidDataException extends Exception {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+    public InvalidDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
