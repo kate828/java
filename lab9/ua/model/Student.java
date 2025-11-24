6bac61aa2069ed235151da277f80a5911699e114
kate828 <katepenziy10@gmail.com>
2025-11-24 09:28:07 +0200

    Create Student.java

---
+++ lab9/ua/model/Student.java
@@ -0,0 +1,32 @@
+package model;
+
+
+public class Student implements Comparable<Student> {
+private int id;
+private String name;
+private int age;
+
+
+public Student(int id, String name, int age) {
+this.id = id;
+this.name = name;
+this.age = age;
+}
+
+
+public int getId() { return id; }
+public String getName() { return name; }
+public int getAge() { return age; }
+
+
+@Override
+public int compareTo(Student o) {
+return Integer.compare(this.id, o.id);
+}
+
+
+@Override
+public String toString() {
+return id + " " + name + " (" + age + ")";
+}
+}
